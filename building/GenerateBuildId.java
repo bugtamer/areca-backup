dbe3c57a817f279b446d2f9cc32e3e4cfa3a6117
bugtamer <bugtamer@outlook.com>
2024-08-28 18:47:46 +0200

    minor javadoc fixes in GenerateBuildId.java

--- building/GenerateBuildId.java
+++ building/GenerateBuildId.java
@@ -4,20 +4,18 @@ import java.util.Random;
 
 
 /**
- * Generates a random BUILD ID for VersionInfos.java
+ * Generates a random <code>BUILD_ID</code> for <code>VersionInfos.java</code>
  * 
  * <p>Shell commands:<p>
  * <ul>
  *   <li>$ <code>javac building/GenerateBuildId.java</code></li>
- *   <li>$ <code>java building/GenerateBuildId</code></li>
+ *   <li>$ <code>java building.GenerateBuildId</code></li>
  * </ul>
  * 
  * <p>Output example:<p>
  * <code>Next Areca's BUILD ID: -7610986468043976209L for src/com/application/areca/version/VersionInfos.java<code>
  * 
  * @see src/com/application/areca/version/VersionInfos.java
- * @since 8.0.0
- * @author bugtamer
  */
 public class GenerateBuildId {
 
