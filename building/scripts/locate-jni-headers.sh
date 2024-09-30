4b11166b91ec3d6f9c41bbe7d58f863b539b0cd1
bugtamer <bugtamer@outlook.com>
2024-09-30 23:46:44 +0200

    add extra paths to locate-jni-headers.sh to find C headers in more places

--- building/scripts/locate-jni-headers.sh
+++ building/scripts/locate-jni-headers.sh
@@ -5,4 +5,23 @@
 # first location:     `head -n 1`
 # remove '/jni.h':    `sed 's/\/jni\.h//g'`
 
-find /usr/lib/jvm -name jni.h | head -n 1 | sed 's/\/jni\.h//g'
+
+# C Headers to find
+JNI=include/jni.h
+JNI_MD=include/linux/jni_md.h
+
+# Getting Areca's directory
+ARECA_DIR=`dirname "$0"`
+ARECA_DIR=`cd "$ARECA_DIR/../.."; pwd`
+
+
+if [[ -e "${ARECA_DIR}/jdk/${JNI}" ]] && [[ -e "${ARECA_DIR}/jdk/${JNI_MD}" ]]; then
+    HEADERS=$ARECA_DIR
+elif [[ -e "${JAVA_HOME}/${JNI}" ]] && [[ -e "${JAVA_HOME}/${JNI_MD}" ]]; then
+    HEADERS=$JAVA_HOME
+else
+    HEADERS=/usr/lib/jvm
+fi
+
+
+find $HEADERS -name jni.h | head -n 1 | sed 's/\/jni\.h//g'
