bba0dbe2a1cddaf2d1b49341ee845b4094ffe5d7
bugtamer <bugtamer@outlook.com>
2024-10-19 16:11:43 +0200

    new releases.sh, fix compile.sh and update build.xml

--- jni/compile.sh
+++ jni/compile.sh
@@ -3,5 +3,16 @@
 # JNI Compilation script
 # If you want to generate the .h file, use : "javah -jni -classpath "lib/areca.jar" com.myJava.file.metadata.posix.jni.wrapper.FileAccessWrapper"
 ########################################################
-gcc -c -fPIC -lacl com_myJava_file_metadata_posix_jni_wrapper_FileAccessWrapper.c -o com_myJava_file_metadata_posix_jni_wrapper_FileAccessWrapper.o
+
+rm libarecafs.so
+
+../building/scripts/install-c-header-dependencies-from-dep-packges.sh
+
+H=$(../building/scripts/locate-jni-headers.sh)
+./header.sh
+
+gcc -c -fPIC -lacl com_myJava_file_metadata_posix_jni_wrapper_FileAccessWrapper.c -o com_myJava_file_metadata_posix_jni_wrapper_FileAccessWrapper.o -I${H} -I${H}/linux
 gcc -shared -lacl -Wl,-soname,libarecafs.so -o libarecafs.so  com_myJava_file_metadata_posix_jni_wrapper_FileAccessWrapper.o
+
+rm com_myJava_file_metadata_posix_jni_wrapper_FileAccessWrapper.o
+rm com_myJava_file_metadata_posix_jni_wrapper_FileAccessWrapper.h
