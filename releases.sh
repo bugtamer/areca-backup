cce4cc71e35d2387ed7878e2c9ea244b886e8431
bugtamer <35902230+bugtamer@users.noreply.github.com>
2025-06-27 00:10:55 +0200

    Improve releases.sh automation

--- releases.sh
+++ releases.sh
@@ -2,23 +2,66 @@
 
 clear
 
+rmdir --ignore-fail-on-non-empty releases/
+mkdir releases/
 
-BRANCH=$(git branch --show-current)
 
-if [ "$BRANCH" != "develop" ]; then
-    echo ERROR: This script can only be run from the develop branch. Current branch: "$BRANCH".
+# WRONG BRANCH GUARD
+
+TARGET_BRANCH=develop
+
+ACTUAL_BRANCH=$(git branch --show-current)
+
+if [ "$ACTUAL_BRANCH" != "${TARGET_BRANCH}" ]; then
+    echo ERROR: This script can only be run from the ${TARGET_BRANCH} branch. Current branch: "$ACTUAL_BRANCH".
     exit 1
 fi
 
+
+# WRONG VERSION GUARD
+
+CSV_VERSION=$(head -2 "docs/developer/builds.csv" | tail -1 | awk -F ';' '{print $2}')
+CURRENT_VERSION=$(head -3 "version.xml" | tail -1 | awk -F '"' '{print $2}')
+
+if [ "$CSV_VERSION" != "$CURRENT_VERSION" ]; then
+    echo ERROR: Latest builds.csv version \($CSV_VERSION\) is not equal to local version.xml \($CURRENT_VERSION\).
+    exit 1
+fi
+
+URL="https://areca-backup.sourceforge.io/version_xml.php"
+VER="releases/site-version.xml"
+wget --no-verbose -O "$VER" "$URL"
+SITE_VERSION=$(head -3 "$VER" | tail -1 | awk -F '"' '{print $2}')
+rm --force "${VER}"
+
+if [ "$CURRENT_VERSION" == "$SITE_VERSION" ]; then
+    echo ERROR: Local version.xml \($CURRENT_VERSION\) is equals to the site version.xml \($SITE_VERSION\).
+    exit 1
+fi
+
+URL="https://raw.githubusercontent.com/bugtamer/areca-backup/refs/heads/${TARGET_BRANCH}/version.xml"
+VER="releases/git-version.xml"
+wget --no-verbose -O "$VER" "$URL"
+GIT_VERSION=$(head -3 "$VER" | tail -1 | awk -F '"' '{print $2}')
+rm --force "${VER}"
+
+if [ "$CURRENT_VERSION" != "$GIT_VERSION" ]; then
+    echo ERROR: Push local changes \($CURRENT_VERSION\) to the remote repo \($GIT_VERSION\).
+    exit 1
+fi
+
+
+# MISSING JDK GUARD
 
 javac -version
 
 if [[ "$?" -ne 0 ]]; then
+    echo ERROR: JDK is not installed or not found.
     exit 1
 fi
 
-rmdir releases/
-mkdir releases/
+
+# BUILD BUNDLES
 
 ant clean
 ant windows-x86-32
@@ -26,10 +69,70 @@ ant windows-x86-64
 ant linux-x86-32
 ant linux-x86-64
 
-cp docs/developer/history.md releases/README.md
 
-cd releases
-zip -9r hashes.zip . -i hashes/*
+# DOWNLOAD SOURCE CODE
+
+cd releases/
+
+URL="https://github.com/bugtamer/areca-backup/archive/refs/heads/develop.zip"
+SRC="areca-$CURRENT_VERSION-src.zip"
+wget --no-verbose -O "$SRC" "$URL"
+
+
+# HASHES
+
+SRC_HASH="hashes/areca-$CURRENT_VERSION-src.txt"
+sha512sum "$SRC"  > "$SRC_HASH"
+sha1sum   "$SRC" >> "$SRC_HASH"
+md5sum    "$SRC" >> "$SRC_HASH"
+
+zip -9r hashes.zip hashes/
 cd ..
 
 rm --dir --force --recursive releases/hashes/
+
+
+# CHANGELOG
+
+HISTORY="docs/developer/history.md"
+found=false
+while IFS= read -r line; do
+    if [[ $line =~ ^## ]]; then
+        if [ "$found" = false ]; then
+            found=true
+        else
+            break
+        fi
+    fi
+    if [ "$found" = true ]; then
+        echo "$line"
+    fi
+done < "$HISTORY" > releases/README.md
+
+
+# UPLOADS
+
+# https://sourceforge.net/p/forge/documentation/File%20Management/
+# https://sourceforge.net/p/forge/documentation/SFTP/
+# https://sourceforge.net/p/forge/documentation/rsync/
+
+RELEASE_DIR=areca-backup-${CURRENT_VERSION}-DEPLOYMENT-TEST
+
+# https://sourceforge.net/p/forge/documentation/SSH%20Keys/
+sftp bugtamer@frs.sourceforge.net <<EOF
+    cd /home/frs/project/areca-backup/areca-stable/
+    mkdir ${RELEASE_DIR}
+    cd  ${RELEASE_DIR}
+    lcd releases/
+    put README.md                                  README.md
+    put hashes.zip                                 hashes.zip
+    put areca-$CURRENT_VERSION-src.zip             areca-$CURRENT_VERSION-src.zip
+    put areca-$CURRENT_VERSION-linux-x86-32.tar.gz areca-$CURRENT_VERSION-linux-x86-32.tar.gz
+    put areca-$CURRENT_VERSION-linux-x86-64.tar.gz areca-$CURRENT_VERSION-linux-x86-64.tar.gz
+    put areca-$CURRENT_VERSION-windows-x86-32.zip  areca-$CURRENT_VERSION-windows-x86-32.zip
+    put areca-$CURRENT_VERSION-windows-x86-64.zip  areca-$CURRENT_VERSION-windows-x86-64.zip
+    cd ..
+    lcd ..
+    put docs/developer/history.md README.md
+    exit
+EOF
